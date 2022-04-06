2ca759dcdc21934ce928cfe6947e1e5ef9c279eb
José Luis Corral López <jcorrallopez8@gmail.com>
2022-04-06 20:49:33 +0200

    6/4/22
    He metido el propio proyecto de java ,lo he conectado con la base de datos y he creado el ddl y dmd en el data modeler

---
+++ com.Proyecto.Final/src/main/java/Main/Main.java
@@ -0,0 +1,38 @@
+package Main;
+
+import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.sql.Statement;
+
+import utlis.utilsDB;
+
+public class Main {
+
+	public Main() {
+
+	}
+
+	public static void main(String[] args) {
+
+		Statement query = utilsDB.conectarBBDD();
+
+		try {
+			if (query.executeUpdate(
+					"INSERT INTO jugador(jugador_id,nombre,apellido,dorsal,apodo,posicion,salario,mVP,anillos,nominacionesAllStar,\r\n"
+							+ "defensorDelAnho,mVPAllStar,finalsMVP,puntosPorPartido,asistenciasPorPartido,rebotesPorPartido,robosPorPartido,\r\n"
+							+ "taponesPorPartido,minutosPorPartido,premioAnotador) VALUES (89,'Prueba','Inserto',78,'ejemplo','Alero',41,4,4,18,0,3,4,30.1,6.3,8.2,1.3,1.1,37.2,1);") > 0) {
+				System.out.println("Jugador introducido correctamente");
+			}
+
+		} catch (SQLIntegrityConstraintViolationException e) {
+
+			System.err.println("Ese usuario ya existe,coge otro avaricioso");
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
